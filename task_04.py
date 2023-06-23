2f8ba5adc8cb8819be3fb71c4548906cc2270794
Андрей Козлов <107209680+AndreKozlov96@users.noreply.github.com>
2023-06-23 18:03:52 +0500

    Delete task_04.py

--- task_04.py
+++
@@ -1,7 +0,0 @@
-words = ['разработка', 'администрирование', 'protocol', 'standard']
-i = 0
-for word in words:
-    i += 1
-    word_bytes = word.encode('utf-8')
-    word_decode = word_bytes.decode('utf-8')
-    print(f'{i}) Encode: type: {type(word_bytes)}, {word_bytes}\nDecode: type: {type(word_decode)}, {word_decode}\n')
