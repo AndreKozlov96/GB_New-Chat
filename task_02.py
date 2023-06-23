af13ea707f91858f79c4c31cef0ddc606f2c7a8b
Андрей Козлов <107209680+AndreKozlov96@users.noreply.github.com>
2023-06-23 18:03:24 +0500

    Delete task_02.py

--- task_02.py
+++
@@ -1,6 +0,0 @@
-words = ['class', 'function', 'method']
-i = 0
-for word in words:
-    i += 1
-    word_bytes = bytes(word, 'ascii')
-    print(f'{i}) type: {type(word_bytes)}, {word_bytes}, len: {len(word_bytes)}')
