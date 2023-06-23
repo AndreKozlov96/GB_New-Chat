b8b492b15eaf113046a7e9f1a10c9683993b4077
Андрей Козлов <107209680+AndreKozlov96@users.noreply.github.com>
2023-06-23 18:03:03 +0500

    Delete task_01.py

--- task_01.py
+++
@@ -1,6 +0,0 @@
-words = ['разработка', 'сокет', 'декоратор']
-i = 0
-for word in words:
-    i += 1
-    code_point = word.encode('unicode_escape')
-    print(f"\n{i}) code_point: {code_point}\ntype: {type(code_point)}, word: {code_point.decode('unicode_escape')}")
