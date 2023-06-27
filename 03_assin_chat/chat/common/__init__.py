c9aeaaccbed227dc6029c5d67efaee2b21dd3a2f
AndreKozlov96 <t53032@mail.ru>
2023-06-27 08:06:57 +0500

    Домашнее задание №3

---
+++ 03_assin_chat/chat/common/__init__.py
@@ -0,0 +1,3 @@
+from .utils import get_message, send_message
+from .veriables import DEFAULT_PORT, DEFAULT_IP_ADDRESS, MAX_CONNECTIONS, MAX_PACKAGE_LENGTH, ENCODING,\
+    ACTION, TIME, USER, ACCOUNT_NAME, PRESENCE, RESPONSE, ERROR
